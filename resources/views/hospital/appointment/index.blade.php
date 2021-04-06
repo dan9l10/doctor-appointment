b91284063278da8c9298e137c3915af6978e12d4
dan9l10 <gagner1322@gmail.com>
2021-04-06 22:36:22 +0300

    fix page appointment

--- resources/views/hospital/appointment/index.blade.php
+++ resources/views/hospital/appointment/index.blade.php
@@ -1,11 +1,12 @@
 @extends('hospital.layouts.app')
 @section('content')
+    <link href="/user/profile/css/profile.css" rel="stylesheet">
     <div class="container">
-    @if ($message = Session::get('success'))
-        <div class="alert alert-success col-md-12 row">
-            <p>{{$message}}</p>
-        </div>
-    @elseif($errors->any())
+        @if ($message = Session::get('success'))
+            <div class="alert alert-success col-md-12 row">
+                <p>{{$message}}</p>
+            </div>
+        @elseif($errors->any())
         <div class="alert alert-danger col-md-12 row">
             <strong>Error!</strong>
             <ul>
@@ -15,50 +16,56 @@
             </ul>
         </div>
     @endif
-    <div class="card">
-        <div class="card-body">
-            <div class="row justify-content-md-center">
-                <div class="col-md-auto">
-                    <img class="col-md-3" src="{{($appointments->avatar)? $appointments->avatar : 'https://html5css.ru/howto/img_avatar2.png'}}" alt="Avatar">
-                    <p class="col-1"><small>{{$appointments->specials->name}}</small></p>
-                    <p class="col-md-9">
-                        <b>
-                            {{$appointments->user->name}} {{$appointments->user->last_name}} {{$appointments->user->patronymic}}
-                        </b>
-                    </p>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <form method="POST" action="{{ route('meet.create',$appointments->user->id) }}">
-            @method('POST')
-            @csrf
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label for="date-appointment">Выберите дату*</label>
-                    <input name="date-appointment" type="date" class="form-control ui-datepicker" id="date-appointment" onchange="refresh({{$appointments->id}})">
-                </div>
-                <div class="form-group">
-                    <label for="inputLogType" class="col-md-6 control-label">Выберите время*</label>
-                    <div>
-                        <div class="btn-group" data-toggle="buttons" id="times">
-
+        <div class="panel">
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="col-md-auto">
+                            <img class="col-md-3" src="{{($appointments->avatar)? $appointments->avatar : 'https://html5css.ru/howto/img_avatar2.png'}}" alt="Avatar">
+                            <p class="col-1"><small>{{$appointments->specials->name}}</small></p>
+                            <p class="col-md-9">
+                                <b>
+                                    {{$appointments->user->name}} {{$appointments->user->last_name}} {{$appointments->user->patronymic}}
+                                </b>
+                            </p>
                         </div>
                     </div>
                 </div>
-                <div class="form-group col-md-11">
-                    <label for="complaint ">Опишите свою жалобу (* от 30 символов)</label>
-                    <textarea name="complaint" rows="3" class="form-control " id="complaint">{{old('complaint')}}</textarea>
+        </div>
+        <div class="panel">
+            <div class="panel-body profile-info">
+                <form method="POST" action="{{ route('meet.create',$appointments->user->id) }}">
+                    @method('POST')
+                    @csrf
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="date-appointment">Выберите дату*</label>
+                            <input name="date-appointment" type="date" class="form-control ui-datepicker" id="date-appointment" onchange="refresh({{$appointments->id}})">
+                        </div>
+                        <div class="form-group">
+                            <label for="inputLogType" class="col-md-6 control-label">Выберите время*</label>
+                            <div class="col-md-6">
+                                <div class="btn-group" data-toggle="buttons" id="times">
+                                    <p>Выберите дату</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-11">
+                            <label for="complaint">Опишите свою жалобу (* от 30 символов)</label>
+                            <textarea name="complaint" rows="3" class="form-control " id="complaint">{{old('complaint')}}</textarea>
+                        </div>
+                    </div>
+                    <div class="col-md-9">
+                        <button type="submit" class="btn btn-primary" id="sendDataAppointment">Create</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+            <div class="panel">
+                <div class="profile-info">
+                    Правила запису
                 </div>
             </div>
-            <div class="col-md-9">
-                <button type="submit" class="btn btn-primary" id="sendDataAppointment">Create</button>
-            </div>
-        </form>
     </div>
-</div>
-
 
 <script
     src="https://code.jquery.com/jquery-3.6.0.min.js"
@@ -77,16 +84,19 @@
                 dataType: 'json',
                 success: function (data) {
                     $('#times').empty();
-                    $.each(data, function (index, element) {
-                        $.each(element.times, function (index, element) {
-                            if (element.status === 1) {
-                                $('#times').append($('<label class="btn btn-primary disabled"><input type="radio" name="time" id="time" disabled>' + element.time + '</label>'));
-                            } else {
-                                $('#times').append($('<label class="btn btn-primary"><input type="radio" name="time" id="time" value="' + element.id + '">' + element.time + '</label>'));
-                            }
-
+                    if (!(Object.keys(data).length == 0)) {
+                        $.each(data, function (index, element) {
+                            $.each(element.times, function (index, element) {
+                                if (element.status === 1) {
+                                    $('#times').append($('<label class="btn btn-primary disabled"><input type="radio" name="time" id="time" disabled>' + element.time + '</label>'));
+                                } else {
+                                    $('#times').append($('<label class="btn btn-primary"><input type="radio" name="time" id="time" value="' + element.id + '">' + element.time + '</label>'));
+                                }
+                            });
                         });
-                    });
+                    }else {
+                        $('#times').append($('<p><b>Времени на запись нет</b></p>'));
+                    }
                 },
                 error: function () {
                     console.log('error');
@@ -94,8 +104,4 @@
             });
         }
     </script>
-
-
-
-
 @endsection
