b172329a9ce795c32ede376fffc1dfe50fc5ca3e
dan9l10 <gagner1322@gmail.com>
2021-04-11 05:17:34 +0300

    added script

--- resources/views/hospital/user/profile.blade.php
+++ resources/views/hospital/user/profile.blade.php
@@ -107,64 +107,7 @@
         src="https://code.jquery.com/jquery-3.6.0.min.js"
         integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
         crossorigin="anonymous"></script>
-    <script>
 
-        function getDataMeet($this){
-            var id = $this.attr('id')
-            console.log(id);
-
-            $.ajax({
-                url: '/meet/'+id,
-                type: 'GET',
-                data: {
-                   id:id
-                },
-                dataType: 'json',
-                success: function (data) {
-                    $('.appointment-info').addClass('hidden');
-                    $('.information-profile').removeClass('hidden').empty().append(`
-                        <div class="panel-body bio-graph-info">
-                            <div class="row">
-                                <p class="col-md-6"><b>Дата: </b>${data.date}</p>
-                                <p class="col-md-6"><b>Лікар: </b>${data.doctor.name} ${data.doctor.last_name} ${data.doctor.patronymic}</p>
-                            </div>
-                            <div class="row">
-                                <p class="col-md-6"><b>Час: </b>${data.times.time}</p>
-                                <p class="col-md-6"><b>Cтатус: </b>${(data.status)? 'Завершено': 'Очікується'}</p>
-                            </div>
-                        </div>
-                        <div class="panel-body bio-graph-info">
-                            <div class="row">
-                                <p class="col-md-12"><b>Скрага: </b>${data.complaint}</p>
-                            </div>
-                            <div class="row">
-                                <p class="col-md-12"><b>Діагноз: </b>${(data.diagnosis)? data.diagnosis : 'Діагнозу не проставлено'}</p>
-                            </div>
-                        </div>
-
-                    `);
-                    if (data.status){
-                        $('.information-profile').append(`<div class="col-md-offset-7"><div class="row"><a onclick="back()" class="btn ml-2">Повернутись назад</a> <a href="" class="btn btn-primary">Записатися повторно</a></div></div>`)
-                    }else{
-                        $('.information-profile').append(`<div class="col-md-offset-9"><div class="row"><a onclick="back()" class="btn ml-2">Повернутись назад</a></div></div>`)
-                    }
-
-                },
-                error: function (data) {
-                    console.log(data);
-                }
-            });
-
-        }
-        function back(){
-            // $('.back-link').on('click',function (){
-                $('.information-profile').addClass('hidden');
-                $('.appointment-info').removeClass('hidden');
-           /* });*/
-        }
-
-
-    </script>
 
 
     @include('scripts.profile.script-update-info-profile-ajax')
